0bc86bdeee9ba0750b54f3f5641d3b103f6970c1
TE-ToshiakiTanaka2 <62982560+TE-ToshiakiTanaka2@users.noreply.github.com>
2020-06-19 17:28:29 +0900

    [Test] (#1)

--- doxygen_github/__init__.py
+++ doxygen_github/__init__.py
@@ -1,2 +1,24 @@
 """ A short description of the project """
 __version__ = "0.1.0"
+
+
+class Sample:
+    """ Sample Class
+    @class Sample Class
+    @brief 説明(簡単)
+    @details 説明（詳細）
+    @warning 警告メッセージ
+    @note メモ
+    """
+    def __init__(self):
+        """
+        @fn __init__()
+        @brief 説明(簡単) [例：クラスの初期化メソッド（コンストラクタ）]
+        @param 引数の説明
+        @return 戻り値の詳細（例：None 戻り値なし）
+        @retval 戻り値の詳細：複数ある場合（例：sum_ab aとbの和（float））
+        @retval 戻り値の詳細：複数ある場合（例：sub_ab aとbの差（float））
+        @details 説明（詳細）
+        @warning 警告メッセージ
+        @note メモ
+        """
