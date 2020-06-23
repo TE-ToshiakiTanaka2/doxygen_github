0880959fd826d42b51171db00e7ae71e3984402b
TE-ToshiakiTanaka2 <TE-ToshiakiTanaka2@users.noreply.github.com>
2020-06-23 06:02:47 +0000

    deploy: 2202b2f7f2c21e7510e5072c3964e813fe5b63b0

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['do_5fthe_5fjob_0',['do_the_job',['../classdoxygen__github_1_1patterns_1_1structural_1_1proxy_1_1Subject.html#af2620f098b7e445666767b8d7d8e42a7',1,'doxygen_github.patterns.structural.proxy.Subject.do_the_job()'],['../classdoxygen__github_1_1patterns_1_1structural_1_1proxy_1_1RealSubject.html#a1186b4ef057c862e432126c155bd6739',1,'doxygen_github.patterns.structural.proxy.RealSubject.do_the_job()'],['../classdoxygen__github_1_1patterns_1_1structural_1_1proxy_1_1Proxy.html#ac889ee0c8faf6a27ed21f169c8fbfaed',1,'doxygen_github.patterns.structural.proxy.Proxy.do_the_job()']]],
+  ['do_5fthe_5fjob_0',['do_the_job',['../classdoxygen__github_1_1patterns_1_1structural_1_1proxy_1_1Proxy.html#ac889ee0c8faf6a27ed21f169c8fbfaed',1,'doxygen_github::patterns::structural::proxy::Proxy']]],
   ['doxygen_5fgithub_1',['doxygen_github',['../namespacedoxygen__github.html',1,'']]],
   ['proxy_2',['proxy',['../namespacedoxygen__github_1_1patterns_1_1structural_1_1proxy.html',1,'doxygen_github::patterns::structural']]]
 ];
--- search/all_4.js
+++ search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['realsubject_6',['RealSubject',['../classdoxygen__github_1_1patterns_1_1structural_1_1proxy_1_1RealSubject.html',1,'doxygen_github::patterns::structural::proxy']]]
+  ['realsubject_6',['RealSubject',['../classdoxygen__github_1_1patterns_1_1structural_1_1proxy_1_1RealSubject.html',1,'doxygen_github.patterns.structural.proxy.RealSubject'],['../classRealSubject.html',1,'RealSubject']]]
 ];
